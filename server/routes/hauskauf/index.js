3e38b4d3240e63ca6180d273b940b023d0ac2115
Benjamin Tanz <btanz@gmx.de>
2015-10-05 14:06:12 +0200

    added template for calc

--- server/routes/hauskauf/index.js
+++ server/routes/hauskauf/index.js
@@ -4,9 +4,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/** GET home page. */
 router.get('/', function(req, res, next) {
   res.render('hauskauf/index');
 });
+
+/** GET mieten oder kaufen. */
+router.get('/mieten-oder-kaufen', function(req, res, next) {
+  res.render('hauskauf/mieten-oder-kaufen');
+});
+
+
+
+
 
 module.exports = router;
