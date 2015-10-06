26c15f4272fb422d302a75720c574e28c74186ba
Benjamin Tanz <btanz@gmx.de>
2015-10-06 12:22:06 +0200

    add input fields to mieten-oder-kaufen

--- client/js/app/MietenOderKaufen.js
+++ client/js/app/MietenOderKaufen.js
@@ -5,24 +5,64 @@
 (function(){
   var MietenOderKaufen = angular.module('MietenOderKaufen', ['rzModule']);
 
-  MietenOderKaufen.controller('MietenOderKaufenController', function($scope){
+  MietenOderKaufen.controller('MietenOderKaufenController', function($scope, $rootScope){
+
+    /** *** CALCULATION FUNCTIONS *** */
+    $scope.calcEquity = function(){
+      return ($scope.equity / 100) * $scope.price;
+    };
+    $scope.calcPriceaddon = function(){
+      return ($scope.priceaddon / 100) * $scope.price;
+    };
+    $scope.calcDebtpay = function(){
+      return ($scope.debtpay / 100) * ($scope.income - ($scope.maintenancePercent / 100) * ($scope.price) / 12);
+    };
+    $scope.calcMaintenanceYear = function(){
+      return ($scope.maintenancePercent / 100) * ($scope.price);
+    };
+    $scope.calcMaintenanceMonth = function(){
+      return ($scope.maintenancePercent / 100) * ($scope.price) / 12;
+    };
+
+
 
     /** initialize values */
-    $scope.price  = 250000;
-    $scope.rent   = 1400;
-    $scope.equity = 30;
-    $scope.income = 1800;
+    $scope.price          = 250000;
+    $scope.rent           = 1400;
+    $scope.equity         = 30;
+    $scope.income         = 1800;
+    $scope.priceaddon     = 7.5;
+    $scope.maintenancePercent = 1.25;
+    $scope.valuedynamic   = 2;
+    $scope.rentdynamic    = 2;
+    $scope.costdynamic    = 1.5;
+    $scope.equityinterest = 1.5;
+    $scope.debtinterest   = 3;
+    $scope.debtpay        = 100;
+
+    //console.log($scope.calcMaintenance());
+
 
     /** slider settings */
     $scope.sliderTranslate = function(value){
       return '';
     };
 
+    /** redraw slider and change angle item once collapsible panels are opened */
+    $('.collapse').on('shown.bs.collapse', function(){
+      $(this).prev().find('i').addClass('fa-angle-down');
+      $rootScope.$broadcast('rzSliderForceRender');
+    });
 
-    /** *** CALCULATION FUNCTIONS *** */
-    $scope.calcEquity = function(){
-      return $scope.equity / 100 * $scope.price + ' €';
-    };
+    $('.collapse').on('hidden.bs.collapse', function(){
+      $(this).prev().find('i').removeClass('fa-angle-down');
+    });
+
+
+
+
+
+
 
     /** run calculations */
     $scope.result = function(){
@@ -31,7 +71,6 @@
 
 
   });
-
 
 
 }());
