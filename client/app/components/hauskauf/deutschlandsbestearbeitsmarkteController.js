5bff0a78aab8c490a0698460b9f8940bf29fe7a2
Benjamin Tanz <btanz@gmx.de>
2015-10-13 16:21:27 +0200

    added map of germany

--- client/app/components/hauskauf/deutschlandsbestearbeitsmarkteController.js
+++ client/app/components/hauskauf/deutschlandsbestearbeitsmarkteController.js
@@ -5,6 +5,118 @@
 
   angular.module('app').controller('deutschlandsbestearbeitsmaerkteController', function($scope, chartConfig){
 
+
+
+
+    var data = [
+      {
+        "hc-key": "de-ni",
+        "value": 0
+      },
+      {
+        "hc-key": "de-sh",
+        "value": 1
+      },
+      {
+        "hc-key": "de-be",
+        "value": 2
+      },
+      {
+        "hc-key": "de-mv",
+        "value": 3
+      },
+      {
+        "hc-key": "de-hb",
+        "value": 4
+      },
+      {
+        "hc-key": "de-sl",
+        "value": 5
+      },
+      {
+        "hc-key": "de-by",
+        "value": 6
+      },
+      {
+        "hc-key": "de-th",
+        "value": 7
+      },
+      {
+        "hc-key": "de-st",
+        "value": 8
+      },
+      {
+        "hc-key": "de-sn",
+        "value": 9
+      },
+      {
+        "hc-key": "de-bb",
+        "value": 10
+      },
+      {
+        "hc-key": "de-nw",
+        "value": 11
+      },
+      {
+        "hc-key": "de-bw",
+        "value": 12
+      },
+      {
+        "hc-key": "de-he",
+        "value": 13
+      },
+      {
+        "hc-key": "de-hh",
+        "value": 14
+      },
+      {
+        "hc-key": "de-rp",
+        "value": 15
+      }
+    ];
+
+    this.config2 = {
+      options: {
+        legend: {
+          enabled: false
+        },
+        mapNavigation: {
+          enabled: true,
+          buttonOptions: {
+            verticalAlign: 'bottom'
+          }
+        },
+        colorAxis: {
+          min: 0
+        },
+        plotOptions: {
+          map: {
+            mapData: Highcharts.maps['countries/de/de-all'],
+            joinBy: 'hc-key',
+          }
+        }
+      },
+      chartType: 'map',
+      title: {
+        text: 'Deutschlandkarte'
+      },
+      series: [{
+        data: data,
+        name: 'Random data',
+        states: {
+          hover: {
+            color: '#BADA55'
+          }
+        },
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}'
+        }
+      }]
+    };
+
+
+
     /** map chart */
     var countries = {
           europe: 'France, Germany, Russia',
@@ -95,6 +207,7 @@
     };
 
     this.config.series[0].allAreas = true;
+
 
 
   });
